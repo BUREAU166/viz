2a36be531bbf10be83ff739484eff8e6b3d5837d
dacsson <sda20036@gmail.com>
2024-11-07 05:01:12 +0300

    add components, context and material ui

--- index.tsx
+++ index.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 const container = document.querySelector('#app');
 const root = createRoot(container);
 
